09b530676a04615325396d7133a94a4dd04ee300
Lab05_etec <Lab05_etec@etec>
2025-03-18 22:40:24 -0300

    Atividade 1

--- Cadastro/src/cadastro/Cadastro.java
+++ Cadastro/src/cadastro/Cadastro.java
@@ -1,21 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cadastro;
+import java.util.Scanner;
 
-/**
- *
- * @author Aluno
- */
 public class Cadastro {
+    
+    public static void Ler(Pessoa arg, int num){
+        
+        Scanner e = new Scanner(System.in);
+        
+        System.out.println("Informe o codigo da pessoa " + num);
+        arg.setCodigo(e.nextInt());
+        
+        System.out.println("Informe o nome da pessoa " + num);
+        arg.setNome(e.next());
+        
+        System.out.println("Informe a idade da pessoa " + num);
+        arg.setIdade(e.nextInt()); 
+    }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Pessoa pessoa1 = new Pessoa();        
+        Pessoa pessoa2 = new Pessoa();
+        Pessoa pessoa3 = new Pessoa();
+        
+        Ler(pessoa1,1);
+        Ler(pessoa2,2);
+        Ler(pessoa3,3);
+        
+        if(pessoa1.getIdade() > pessoa2.getIdade() && pessoa1.getIdade() > pessoa3.getIdade()){
+            System.out.println("A pessoa com a maior idade é: " + pessoa1.getNome() + " com " + pessoa1.getIdade() + " anos");
+        }else if(pessoa2.getIdade() > pessoa1.getIdade() && pessoa2.getIdade() > pessoa3.getIdade()){
+            System.out.println("A pessoa com a maior idade é: " + pessoa2.getNome() + " com " + pessoa2.getIdade() + " anos");
+        }else if(pessoa3.getIdade() > pessoa1.getIdade() && pessoa3.getIdade() > pessoa2.getIdade()){
+            System.out.println("A pessoa com a maior idade é: " + pessoa3.getNome() + " com " + pessoa3.getIdade() + " anos");
+        }else{
+            System.out.println("As pessoas tem idade igual!");
+        }    
     }
     
 }
---
+++ Cadastro/src/cadastro/Pessoa.java
@@ -0,0 +1,33 @@
+package cadastro;
+
+public class Pessoa {
+    private int codigo;
+    private String nome;
+    private int idade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    
+    
+}
