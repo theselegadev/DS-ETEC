7bc9e89e66c82206794bd9b805e6aca859b794f3
Lab05_etec <Lab05_etec@etec>
2025-04-07 21:34:02 -0300

    Vetor em java

---
+++ Vetor/src/vetor/Vetor.java
@@ -0,0 +1,13 @@
+package vetor;
+
+public class Vetor {
+
+    public static void main(String[] args) {
+        int[] lista = {10,20,30,40,50};
+        
+        for(int i = 0; i < lista.length; i++){
+            System.out.println("lista["+i+"]="+lista[i]);
+        }
+    }
+    
+}
